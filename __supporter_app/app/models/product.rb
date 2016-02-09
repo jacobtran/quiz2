b3c8ec6a8a562cf2562e26abd5c9c59c8fafa0ed
Jacob Tran <jacobtran@gmail.com>
2016-02-09 14:08:32 -0800

    quiz2 files
    quiz2 files

--- __supporter_app/app/models/product.rb
+++
@@ -1,7 +0,0 @@
-class Product < ActiveRecord::Base
-  validates :name, presence: true,
-                   length: {minimum: 3},
-                   uniqueness: {case_sensitive: false,
-                                message:        "already exists!"}
-  validates :price, numericality: {greater_than_or_equal_to: 0}
-end
